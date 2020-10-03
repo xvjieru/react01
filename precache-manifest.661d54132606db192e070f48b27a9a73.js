c913e9c3b873450ea28613a7bcf6a6d7511cf7d9
xvjieru <1320836265@qq.com>
2020-10-03 21:00:38 +0800

    react

---
+++ precache-manifest.661d54132606db192e070f48b27a9a73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6058f5a68606861f764a29b9429cc4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32bd928cdda712f6696",
+    "url": "/static/js/2.1413a434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1413a434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3dc76852289bea6391",
+    "url": "/static/js/main.98e42437.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
